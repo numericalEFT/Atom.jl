82fd978c3438349be80d8899c638e202ec0e277c
Kun Chen <chenkun0228@gmail.com>
2022-06-16 12:40:33 -0400

    initialization

--- docs/make.jl
+++ docs/make.jl
@@ -6,19 +6,19 @@ DocMeta.setdocmeta!(Atom, :DocTestSetup, :(using Atom); recursive=true)
 makedocs(;
     modules=[Atom],
     authors="Kun Chen",
-    repo="https://github.com/kunyuan/Atom.jl/blob/{commit}{path}#{line}",
+    repo="https://github.com/numericalEFT/Atom.jl/blob/{commit}{path}#{line}",
     sitename="Atom.jl",
     format=Documenter.HTML(;
         prettyurls=get(ENV, "CI", "false") == "true",
-        canonical="https://kunyuan.github.io/Atom.jl",
-        assets=String[],
+        canonical="https://numericaleft.github.io/Atom.jl",
+        assets=String[]
     ),
     pages=[
         "Home" => "index.md",
-    ],
+    ]
 )
 
 deploydocs(;
-    repo="github.com/kunyuan/Atom.jl",
-    devbranch="main",
+    repo="github.com/numericalEFT/Atom.jl",
+    devbranch="master"
 )
